4498d023cf890ddc9ac58bc9be0217f91df588c5
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-05-02 20:42:03 -0400

    Update ImageGallery.tsx

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import ImageCard from "../ImageCard/ImageCard";
 import styles from "./ImageGallery.module.css";
 
-const ImageGallery = ({ images, onImageClick }) => {
+type Image = {
+  id: string;
+  urls: {
+    small: string;
+  };
+  alt_description: string;
+  user: {
+    name: string;
+  };
+  views: number;
+};
+
+type ImageGalleryProps = {
+  images: Image[];
+  onImageClick: (image: Image) => void;
+};
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ images, onImageClick }) => {
   return (
     <ul className={styles.gallery}>
       {images.map((image) => (
